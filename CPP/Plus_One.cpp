190d1193b8345f14a68ed1ba53ffc745315ffcfd
Naitik Gupta <naitikgupta006@gmail.com>
2025-01-15 00:12:08 +0530

    Code Debugged

--- CPP/Plus_One.cpp
+++ CPP/Plus_One.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n= digits.size();
-        if (digits[n-1]==9){
-            if(digits[0]==9){
-                digits.push_back(0);
-                n++;
-                for (int i=1;i<n-1;i++){
-                    digits[i]=0;
-                }
-                digits[0]=1;
-            }
-            else{
-                digits[n-1]=0;
-                digits[0]++;
-                for (int i=;i<n-1;i++){
-                    if(digits[i]==9){
-                        digits[i]==0;
-                    }
-                    else{
-                        digits[i]=digits[i]+1;
-                    }
-                }
+        int n = digits.size();
+       
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0; 
+            } else {
+                digits[i]++; 
+                return digits; 
             }
         }
-        else{
-            digits[n-1]++;
-        }
+        
+        digits.insert(digits.begin(), 1);
         return digits;
     }
 };
